06efbe62065aeed72cc5740e964b61730e5761e8
Tomas Doran <tdoran@yelp.com>
2013-11-09 13:36:34 +0000

    Remove manifests

--- manifests/config.pp
+++
@@ -1,6 +0,0 @@
-# == Class sshkey_utilities::config
-#
-# This class is called from sshkey_utilities
-#
-class sshkey_utilities::config {
-}
--- manifests/install.pp
+++
@@ -1,9 +0,0 @@
-# == Class sshkey_utilities::intall
-#
-class sshkey_utilities::install {
-  include sshkey_utilities::params
-
-  package { $sshkey_utilities::params::package_name:
-    ensure => present,
-  }
-}
